c429d89b71ba9e9ec08bebaf4ba5b936b8733963
LucasCancio <lucas.cancio7@gmail.com>
2021-01-10 19:43:28 -0300

    Commitando projeto

---
+++ README.md
@@ -0,0 +1,13 @@
+# ⛅ Olha a chuva! 🌧
+
+![inicio](docs/inicio-print.PNG)
+![favoritos](docs/favoritos-print.PNG)
+![detalhes](docs/detalhes-print.PNG)
+
+Projeto que consulta a previsão do tempo via API
+
+## Digital Innovation One
+
+<img src="docs/dio-logo.png" width="100" />
+
+[Clique aqui para se inscrever na Digital Innovation One](https://digitalinnovation.one/sign-up?ref=H395IYS4Z6)
---
+++ olha-a-chuva/src/app/app.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+
+import * as moment from 'moment-timezone';
+import 'moment/locale/pt-br';
+
+interface MenuItem {
+  isSelected: boolean;
+  link: string;
+  title: string;
+}
+
+@Component({
+  selector: 'jv-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent implements OnInit {
+  menuItems: MenuItem[];
+
+  constructor() {
+    moment.locale('pt-br');
+  }
+
+  ngOnInit() {
+    this.menuItems = [
+      {
+        isSelected: true,
+        link: '/',
+        title: 'Início',
+      },
+      {
+        isSelected: false,
+        link: '/bookmarks',
+        title: 'Favoritos',
+      },
+    ];
+  }
+
+  changeActiveMenu(item: MenuItem) {
+    this.clearActivesMenus();
+    item.isSelected = true;
+  }
+
+  private clearActivesMenus() {
+    this.menuItems.forEach((i) => (i.isSelected = false));
+  }
+}
